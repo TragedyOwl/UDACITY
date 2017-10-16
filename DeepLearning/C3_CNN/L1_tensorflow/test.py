638768c5a4071859ff3492aab42ae43acc5ddd7c
TragedyOwl <598249974@qq.com>
2017-10-16 16:14:54 +0800

    +Mini-batch 这个将数据进行分P处理的程序很有借鉴性

---
+++ DeepLearning/C3_CNN/L1_tensorflow/test.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import time
+
+# Create TensorFlow object called tensor
+hello_constant = tf.constant('Hello World!')
+
+with tf.Session() as sess:
+    # Run the tf.constant operation in the session
+    output = sess.run(hello_constant)
+    print(output)
+
+
+
+x = tf.placeholder(tf.string)
+y = tf.placeholder(tf.int32)
+z = tf.placeholder(tf.float32)
+
+with tf.Session() as sess:
+    output = sess.run(x, feed_dict={x: 'Test String', y: 123, z: 45.67})
+    print(output)
+
+
+
+
+
